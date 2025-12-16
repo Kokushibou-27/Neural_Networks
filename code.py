ea1c3bb9660d01ffca50cee9c02ce32df4f9f673
Siddharth Ganesh <siddharthg1703@gmail.com>
2025-12-16 16:04:06 +0530

    done

--- code.py
+++ code.py
@@ -123,3 +123,22 @@ def gradient_descent(X, Y, alpha, epochs):
     return W1, b1, W2, b2, W3, b3, costs
 
 W1, b1, W2, b2, W3, b3, costs = gradient_descent(X_train.T, Y_train, 0.1, 100) 
+
+
+
+def make_predictions(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+    predictions = get_predictions(A2)
+    return predictions
+
+def test_prediction(index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+    
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
