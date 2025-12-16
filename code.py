3c33d601da28bc5afc97ad9f5a6c2761d2fc915a
Siddharth Ganesh <siddharthg1703@gmail.com>
2025-12-16 16:09:30 +0530

    .

--- code.py
+++ code.py
@@ -124,16 +124,14 @@ def gradient_descent(X, Y, alpha, epochs):
 
 W1, b1, W2, b2, W3, b3, costs = gradient_descent(X_train.T, Y_train, 0.1, 100) 
 
-
-
-def make_predictions(X, W1, b1, W2, b2):
-    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
-    predictions = get_predictions(A2)
+def make_predictions(X, W1, b1, W2, b2, W3, b3):
+    _, _, _, _, _, A3 = forward_prop(W1, b1, W2, b2, W3, b3, X)
+    predictions = get_predictions(A3)
     return predictions
 
-def test_prediction(index, W1, b1, W2, b2):
+def test_prediction(index, W1, b1, W2, b2, W3, b3):
     current_image = X_train[:, index, None]
-    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2, W3, b3)
     label = Y_train[index]
     print("Prediction: ", prediction)
     print("Label: ", label)
@@ -142,3 +140,7 @@ def test_prediction(index, W1, b1, W2, b2):
     plt.gray()
     plt.imshow(current_image, interpolation='nearest')
     plt.show()
+
+# Update the accuracy check on the dev set
+dev_predictions = make_predictions(X_dev, W1, b1, W2, b2, W3, b3)
+print(get_accuracy(dev_predictions, Y_dev))
